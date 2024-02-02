ae7492902cad638fa9c752d5263792bcd7fdd481
Ojonugwa Alikali <alikali.ojonugwajustice@gmail.com>
2024-02-02 06:03:21 +0100

    Fix icon issues

--- src/components/EventHighlight/index.tsx
+++ src/components/EventHighlight/index.tsx
@@ -12,63 +12,73 @@ const EventHighlightComponent: React.FC = () => {
 
           <ul>
             <li>
-              <svg width="32" height="33" viewBox="0 0 32 33" fill="none" xmlns="http://www.w3.org/2000/svg">
-                <path
-                  d="M6.56641 18.0494L11.8181 23.301L24.9472 10.1719"
-                  stroke="#4EA59D"
-                  stroke-width="4"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                />
-              </svg>
+              <span>
+                <svg width="32" height="33" viewBox="0 0 32 33" fill="none" xmlns="http://www.w3.org/2000/svg">
+                  <path
+                    d="M6.56641 18.0494L11.8181 23.301L24.9472 10.1719"
+                    stroke="#4EA59D"
+                    stroke-width="4"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                  />
+                </svg>
+              </span>
               <span>Expert-led sessions</span>
             </li>
             <li>
-              <svg width="32" height="33" viewBox="0 0 32 33" fill="none" xmlns="http://www.w3.org/2000/svg">
-                <path
-                  d="M6.56641 18.0494L11.8181 23.301L24.9472 10.1719"
-                  stroke="#4EA59D"
-                  stroke-width="4"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                />
-              </svg>
+              <span>
+                <svg width="32" height="33" viewBox="0 0 32 33" fill="none" xmlns="http://www.w3.org/2000/svg">
+                  <path
+                    d="M6.56641 18.0494L11.8181 23.301L24.9472 10.1719"
+                    stroke="#4EA59D"
+                    stroke-width="4"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                  />
+                </svg>
+              </span>
               <span>Interactive discussions and Q/A Sessions</span>
             </li>
             <li>
-              <svg width="32" height="33" viewBox="0 0 32 33" fill="none" xmlns="http://www.w3.org/2000/svg">
-                <path
-                  d="M6.56641 18.0494L11.8181 23.301L24.9472 10.1719"
-                  stroke="#4EA59D"
-                  stroke-width="4"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                />
-              </svg>
+              <span>
+                <svg width="32" height="33" viewBox="0 0 32 33" fill="none" xmlns="http://www.w3.org/2000/svg">
+                  <path
+                    d="M6.56641 18.0494L11.8181 23.301L24.9472 10.1719"
+                    stroke="#4EA59D"
+                    stroke-width="4"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                  />
+                </svg>
+              </span>
               <span>Networking Opportunities</span>
             </li>
             <li>
-              <svg width="32" height="33" viewBox="0 0 32 33" fill="none" xmlns="http://www.w3.org/2000/svg">
-                <path
-                  d="M6.56641 18.0494L11.8181 23.301L24.9472 10.1719"
-                  stroke="#4EA59D"
-                  stroke-width="4"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                />
-              </svg>
+              <span>
+                <svg width="32" height="33" viewBox="0 0 32 33" fill="none" xmlns="http://www.w3.org/2000/svg">
+                  <path
+                    d="M6.56641 18.0494L11.8181 23.301L24.9472 10.1719"
+                    stroke="#4EA59D"
+                    stroke-width="4"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                  />
+                </svg>
+              </span>
               <span>Feedback and assessment</span>
             </li>
             <li>
-              <svg width="32" height="33" viewBox="0 0 32 33" fill="none" xmlns="http://www.w3.org/2000/svg">
-                <path
-                  d="M6.56641 18.0494L11.8181 23.301L24.9472 10.1719"
-                  stroke="#4EA59D"
-                  stroke-width="4"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                />
-              </svg>
+              <span>
+                <svg width="32" height="33" viewBox="0 0 32 33" fill="none" xmlns="http://www.w3.org/2000/svg">
+                  <path
+                    d="M6.56641 18.0494L11.8181 23.301L24.9472 10.1719"
+                    stroke="#4EA59D"
+                    stroke-width="4"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                  />
+                </svg>
+              </span>
               <span>Collaborative activities</span>
             </li>
           </ul>
